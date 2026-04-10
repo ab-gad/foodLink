d3486e70880a530d4d22b68ae1ba2eed4282dfad
ab-gad <abdulrahman.gad@zellige.net>
2026-04-10 02:13:12 +0200

    chore(repo): remove welcome components

--- apps/admin/src/app/app.ts
+++ apps/admin/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'admin-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected title = 'admin';
--- libs/donor/feature-shell/src/lib/donor-feature-shell/donor-feature-shell.ts
+++ libs/donor/feature-shell/src/lib/donor-feature-shell/donor-feature-shell.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'lib-donor-feature-shell',
   imports: [],
   templateUrl: './donor-feature-shell.html',
   styleUrl: './donor-feature-shell.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DonorFeatureShell {}
+export class DonorFeatureShell { }
